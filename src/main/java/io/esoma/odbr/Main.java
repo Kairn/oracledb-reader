681fd158bec34ead67e23f44773cb0a739ba8f3e
Eddy Soma <esoma92.dev@gmail.com>
2019-06-22 02:04:58 -0500

    setup jdbc connection with db properties

--- src/main/java/io/esoma/odbr/Main.java
+++ src/main/java/io/esoma/odbr/Main.java
@@ -1,9 +1,40 @@
 package io.esoma.odbr;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ClassNotFoundException {
+
+		// Initialize Oracle driver
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		// DB connection
+		Connection conn = null;
+
+		try {
+			ClassLoader cl = Thread.currentThread().getContextClassLoader();
+			InputStream in = cl.getResourceAsStream("config.properties");
+
+			Properties props = new Properties();
+			props.load(in);
+
+			// Get connection
+			conn = DriverManager.getConnection(props.getProperty("url"), props.getProperty("username"),
+					props.getProperty("password"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+
+		if (conn != null) {
+			System.out.println(conn.toString());
+		} else {
+			System.out.println("Connection failed");
+		}
 
 	}
 
